c05b35a726474ea2485cb5df3329b894f8725d13
Nikhil Patel <nikhilpatel26150@gmail.com>
2023-05-31 23:48:13 +0530

    update app.js file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Article, Brand, CTA } from './components'
+
+const App = () => {
+  return (
+    <div>
+        <h1>GPT-4</h1>
+    </div>
+  )
+}
+
+export default App
